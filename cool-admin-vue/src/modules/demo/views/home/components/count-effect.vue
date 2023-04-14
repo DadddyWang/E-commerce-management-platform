415057f3d6f6627ff563ecc54166330bcfd6a5a7
Alkaidcc <alkaidcc@qq.com>
2023-04-14 14:48:53 +0800

    feat: deliver

--- cool-admin-vue/src/modules/demo/views/home/components/count-effect.vue
+++ cool-admin-vue/src/modules/demo/views/home/components/count-effect.vue
@@ -3,12 +3,12 @@
 		<div class="card">
 			<div class="card__header">
 				<span class="label">未发货商品数</span>
-				<span class="value">34</span>
+				<span class="value">{{ notDelivered }}</span>
 			</div>
 
 			<div class="card__container">
 				<span class="label">未发货商品比例</span>
-				<el-progress :percentage="10" :stroke-width="8" />
+				<el-progress :percentage="notDeliveredPercent" :stroke-width="8" />
 			</div>
 
 			<div class="card__footer"></div>
@@ -16,7 +16,27 @@
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { request } from "/@/cool/service/request";
+const fetchNotDelivered = async () => {
+	const { delivery } = (await request.get("/dev/dashboard/delivery")) as any;
+	delivery.forEach((element: any) => {
+		if (element.shipmentStatus === 0) {
+			notDelivered.value = Number(element.number);
+		} else {
+			delivered.value = Number(element.number);
+		}
+	});
+	notDeliveredPercent.value = (notDelivered.value / (notDelivered.value + delivered.value)) * 100;
+};
+const notDeliveredPercent = ref(0);
+const notDelivered = ref(0);
+const delivered = ref(0);
+onMounted(() => {
+	fetchNotDelivered();
+});
+</script>
 
 <style lang="scss" scoped>
 .count-effect {
